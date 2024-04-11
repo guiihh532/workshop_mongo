7ee59bcdca95ae2f4279c67998a02610a7da1c20
Guilherme Messias <guiihh532@gmail.com>
2024-04-11 19:50:43 -0300

    userResource complete

---
+++ src/main/java/com/gomlek/workshop_mongo/domain/User.java
@@ -0,0 +1,61 @@
+package com.gomlek.workshop_mongo.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private String nome;
+    private String email;
+
+    public User(){
+    }
+
+    public User(String id, String nome, String email) {
+        super();
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+}
