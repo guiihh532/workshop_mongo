7c0d70b99a316306414de31049e6be71e8fbd009
Guilherme Messias <guiihh532@gmail.com>
2024-04-11 20:42:11 -0300

    conectando com ao mongodb com repository e service

--- src/main/java/com/gomlek/workshop_mongo/domain/User.java
+++ src/main/java/com/gomlek/workshop_mongo/domain/User.java
@@ -1,12 +1,19 @@
 package com.gomlek.workshop_mongo.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.Objects;
 
+
+@Document(collection = "user")
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
     private String id;
     private String nome;
     private String email;
--- src/main/java/com/gomlek/workshop_mongo/resources/UserResource.java
+++ src/main/java/com/gomlek/workshop_mongo/resources/UserResource.java
@@ -1,6 +1,8 @@
 package com.gomlek.workshop_mongo.resources;
 
 import com.gomlek.workshop_mongo.domain.User;
+import com.gomlek.workshop_mongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,14 +17,13 @@ import java.util.List;
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
     public ResponseEntity<List<User>> findAll() {
-        User maria = new User("1", "maria", "maria@gmail.com");
-        User alex = new User("2", "alex", "alex@gmail.com");
-        User duda = new User("3", "duda", "duda@gmail.com");
-        List<User> list = new ArrayList<User>();
-        list.addAll(Arrays.asList(maria, alex, duda));
 
+        List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
